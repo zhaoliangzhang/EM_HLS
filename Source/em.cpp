8896f6aa27d28486ca9b6a6ba7b05bc9f4dc3f8e
zhaoliangzhang98 <zhaoliang.zhang98@gmail.com>
2020-12-30 18:13:59 +0800

    change compare tree

--- Source/em.cpp
+++ Source/em.cpp
@@ -12,264 +12,174 @@ void GetData(DATA _data[MAX_MODEL_NUM*3], hls::stream<DATA> &data, hls::stream<D
     }
 }
 
-void CalProb(hls::stream<DATA> &data, hls::stream<PROB> prob[MAX_MODEL_NUM], PRIOR priors[MAX_MODEL_NUM], MEANS means[MAX_MODEL_NUM][DIM], VARS vars[MAX_MODEL_NUM][DIM]){
+void CalProb(hls::stream<DATA> &data, hls::stream<Vector<MAX_MODEL_NUM, PROB> > &probs, PRIOR priors[MAX_MODEL_NUM], MEANS means[MAX_MODEL_NUM][DIM], VARS vars[MAX_MODEL_NUM][DIM]){
     CalPROBF:for(uint32_t n=0; n<DATA_NUM; n++) {
-        //#pragma HLS PIPELINE
         DATA sample[3];
         for(uint32_t i=0; i<3; i++){
             data.read(sample[i]);
         }
+        Vector<MAX_MODEL_NUM, PROB> local_probs;
         CalPROB:for(uint32_t i=0; i<MAX_MODEL_NUM; i++) {
             #pragma HLS PIPELINE
-            PROB tmp_prob = 1;
+            local_probs.vec[i] = 1;
             probability:for (uint32_t d = 0; d < DIM; d++) {
-                tmp_prob *= genhao_er_pai_fenzhiyi * vars[i][d];
-                tmp_prob *= exp( (-0.5) * (sample[d] - means[i][d]) * (sample[d] - means[i][d]) * vars[i][d]);
+                local_probs.vec[i] *= genhao_er_pai_fenzhiyi * vars[i][d];
+                local_probs.vec[i] *= exp( (-0.5) * (sample[d] - means[i][d]) * (sample[d] - means[i][d]) * vars[i][d]);
             }
-            tmp_prob *= priors[i];
-            prob[i].write(tmp_prob);
+            local_probs.vec[i] *= priors[i];
         }
+        probs.write(local_probs);
     }
 }
 
-void GetLabel_128(hls::stream<PROB> dis[MAX_MODEL_NUM], hls::stream<PROB> local_dis_128[128], hls::stream<uint32_t> label_128[128]) {
+void GetLabel_128(hls::stream<Vector<MAX_MODEL_NUM, PROB> > &probs, hls::stream<Vector<128, PROB> > &dis_128, hls::stream<Vector<128, uint32_t> > &label_128) {
     for(uint32_t n=0; n<DATA_NUM; n++) {
+        Vector<MAX_MODEL_NUM, PROB> local_probs;
+        Vector<128, PROB> local_dis_128;
+        Vector<128, uint32_t> local_label_128;
+        probs.read(local_probs);
         for(uint32_t i=0; i<128; i++){
-            PROB tmp1, tmp2;
-            dis[i].read(tmp1);
-            dis[i+128].read(tmp2);
-            if(tmp1 > tmp2){
-                local_dis_128[i].write(tmp1);
-                label_128[i].write(i);
+            if(local_probs.vec[i] > local_probs.vec[i+128]){
+                local_dis_128.vec[i] = local_probs.vec[i];
+                local_label_128.vec[i] = i;
             } else {
-                local_dis_128[i].write(tmp2);
-                label_128[i].write(i+128);
+                local_dis_128.vec[i] = local_probs.vec[i+128];
+                local_label_128.vec[i] = i+128;
             }
         }
+        dis_128.write(local_dis_128);
+        label_128.write(local_label_128);
     }
 }
 
-void GetLabel_64(hls::stream<PROB> local_dis_128[128], hls::stream<uint32_t> label_128[128], hls::stream<PROB> local_dis_64[64], hls::stream<uint32_t> label_64[64]) {
+template<uint32_t size1, uint32_t size2>
+void GetLabel(hls::stream<Vector<size1, PROB> > &dis1, hls::stream<Vector<size1, uint32_t> > &label1, hls::stream<Vector<size2, PROB> > &dis2, hls::stream<Vector<size2, uint32_t> > &label2) {
     for(uint32_t n=0; n<DATA_NUM; n++) {
-        for(uint32_t i=0; i<64; i++) {
-            PROB tmp, tmp1;
-            uint32_t lab, lab1;
-            local_dis_128[i].read(tmp);
-            local_dis_128[i+64].read(tmp1);
-            label_128[i].read(lab);
-            label_128[i+64].read(lab1);
-            if(tmp > tmp1) {
-                local_dis_64[i].write(tmp);
-                label_64[i].write(lab);
+        Vector<size1, PROB> local_dis1;
+        Vector<size1, uint32_t> local_label1;
+        dis1.read(local_dis1);
+        label1.read(local_label1);
+        Vector<size2, PROB> local_dis2;
+        Vector<size2, uint32_t> local_label2;
+        for(uint32_t i=0; i<size2; i++) {
+            if(local_dis1.vec[i] > local_dis1.vec[i+size2]) {
+                local_dis2.vec[i] = local_dis1.vec[i];
+                local_label2.vec[i] = local_label1.vec[i];
             } else {
-                local_dis_64[i].write(tmp1);
-                label_64[i].write(lab1);
+                local_dis2.vec[i] = local_dis1.vec[i+size2];
+                local_label2.vec[i] = local_label1.vec[i+size2];
             }
         }
+        dis2.write(local_dis2);
+        label2.write(local_label2);
     }
 }
 
-void GetLabel_32(hls::stream<PROB> local_dis_64[64], hls::stream<uint32_t> label_64[64], hls::stream<PROB> local_dis_32[32], hls::stream<uint32_t> label_32[32]) {
+void GetLabel_1(hls::stream<Vector<2, PROB> > &dis_2, hls::stream<Vector<2, uint32_t> > &label_2, hls::stream<Vector<MAX_MODEL_NUM, PROB> > &P) {
     for(uint32_t n=0; n<DATA_NUM; n++) {
-        for(uint32_t i=0; i<32; i++) {
-            PROB tmp, tmp1;
-            uint32_t lab, lab1;
-            local_dis_64[i].read(tmp);
-            local_dis_64[i+32].read(tmp1);
-            label_64[i].read(lab);
-            label_64[i+32].read(lab1);
-            if(tmp > tmp1) {
-                local_dis_32[i].write(tmp);
-                label_32[i].write(lab);
-            } else {
-                local_dis_32[i].write(tmp1);
-                label_32[i].write(lab1);
-            }
-        }
-    }
-}
+        Vector<2, PROB> local_dis_2;
+        Vector<2, uint32_t> local_label_2;
+        dis_2.read(local_dis_2);
+        label_2.read(local_label_2);
 
-void GetLabel_16(hls::stream<PROB> local_dis_32[32], hls::stream<uint32_t> label_32[32], hls::stream<PROB> local_dis_16[16], hls::stream<uint32_t> label_16[16]) {
-    for(uint32_t n=0; n<DATA_NUM; n++) {
-        for(uint32_t i=0; i<16; i++) {
-            PROB tmp, tmp1;
-            uint32_t lab, lab1;
-            local_dis_32[i].read(tmp);
-            local_dis_32[i+16].read(tmp1);
-            label_32[i].read(lab);
-            label_32[i+16].read(lab1);
-            if(tmp > tmp1) {
-                local_dis_16[i].write(tmp);
-                label_16[i].write(lab);
-            } else {
-                local_dis_16[i].write(tmp1);
-                label_16[i].write(lab1);
-            }
-        }
-    }
-}
-
-void GetLabel_8(hls::stream<PROB> local_dis_16[16], hls::stream<uint32_t> label_16[16], hls::stream<PROB> local_dis_8[8], hls::stream<uint32_t> label_8[8]) {
-    for(uint32_t n=0; n<DATA_NUM; n++) {
-        for(uint32_t i=0; i<8; i++) {
-            PROB tmp, tmp1;
-            uint32_t lab, lab1;
-            local_dis_16[i].read(tmp);
-            local_dis_16[i+8].read(tmp1);
-            label_16[i].read(lab);
-            label_16[i+8].read(lab1);
-            if(tmp > tmp1) {
-                local_dis_8[i].write(tmp);
-                label_8[i].write(lab);
-            } else {
-                local_dis_8[i].write(tmp1);
-                label_8[i].write(lab1);
-            }
-        }
-    }
-}
-
-void GetLabel_4(hls::stream<PROB> local_dis_8[8], hls::stream<uint32_t> label_8[8], hls::stream<PROB> local_dis_4[4], hls::stream<uint32_t> label_4[4]) {
-    for(uint32_t n=0; n<DATA_NUM; n++) {
-        for(uint32_t i=0; i<4; i++) {
-            PROB tmp, tmp1;
-            uint32_t lab, lab1;
-            local_dis_8[i].read(tmp);
-            local_dis_8[i+4].read(tmp1);
-            label_8[i].read(lab);
-            label_8[i+4].read(lab1);
-            if(tmp > tmp1) {
-                local_dis_4[i].write(tmp);
-                label_4[i].write(lab);
-            } else {
-                local_dis_4[i].write(tmp1);
-                label_4[i].write(lab1);
-            }
-        }
-    }
-}
-
-void GetLabel_2(hls::stream<PROB> local_dis_4[4], hls::stream<uint32_t> label_4[4], hls::stream<PROB> local_dis_2[2], hls::stream<uint32_t> label_2[2]) {
-    for(uint32_t n=0; n<DATA_NUM; n++) {
-        for(uint32_t i=0; i<2; i++) {
-            PROB tmp, tmp1;
-            uint32_t lab, lab1;
-            local_dis_4[i].read(tmp);
-            local_dis_4[i+2].read(tmp1);
-            label_4[i].read(lab);
-            label_4[i+2].read(lab1);
-            if(tmp > tmp1) {
-                local_dis_2[i].write(tmp);
-                label_2[i].write(lab);
-            } else {
-                local_dis_2[i].write(tmp1);
-                label_2[i].write(lab1);
-            }
-        }
-    }
-}
-
-void GetLabel_1(hls::stream<PROB> local_dis_2[2], hls::stream<uint32_t> label_2[2], hls::stream<PROB> P[MAX_MODEL_NUM]) {
-    for(uint32_t n=0; n<DATA_NUM; n++) {
         uint32_t p;
-        PROB local_tmp, local_tmp1;
-        local_dis_2[0].read(local_tmp);
-        local_dis_2[1].read(local_tmp1);
-        if(local_tmp>local_tmp1) {
-            label_2[0].read(p);
+
+        Vector<MAX_MODEL_NUM, PROB> local_P;
+        if(local_dis_2.vec[0]>local_dis_2.vec[1]) {
+            p = local_label_2.vec[0];
         } else {
-            label_2[1].read(p);
+            p = local_label_2.vec[1];
         }
         for(uint32_t i=0; i<MAX_MODEL_NUM; i++) {
             if(i==p) {
-                P[i].write(1);
+                local_P.vec[i] = 1.0;
             } else {
-                P[i].write(0);
+                local_P.vec[i] = 0.0;
             }
         }
+        P.write(local_P);
     }
 }
 
-void AccumProb(hls::stream<PROB> prob[MAX_MODEL_NUM], hls::stream<PROB> P[MAX_MODEL_NUM]) {
-    PROB local_prob[MAX_MODEL_NUM];
-    #pragma HLS ARRAY_PARTITION variable=local_prob block factor=16 dim=1
+void AccumProb(hls::stream<Vector<MAX_MODEL_NUM, PROB> > &probs, hls::stream<Vector<MAX_MODEL_NUM, PROB> > &P) {
+    Vector<MAX_MODEL_NUM, PROB> local_probs;
+    #pragma HLS ARRAY_PARTITION variable=local_probs block factor=16 dim=1
 
     Accum1:for(uint32_t n=0; n<DATA_NUM; n++) {
         PROB sum = 0;
+        probs.read(local_probs);
         Accum2:for(uint32_t i=0; i<MAX_MODEL_NUM; i++) {
-            prob[i].read(local_prob[i]);
-            sum += local_prob[i];
+            sum += local_probs.vec[i];
         }
         Accum3:for(uint32_t i=0; i<MAX_MODEL_NUM; i++) {
-            local_prob[i] /= sum;
-            P[i].write(local_prob[i]);
+            local_probs.vec[i] /= sum;
         }
+        P.write(local_probs);
     }
 }
 
-void ProcessProb(hls::stream<PROB> probs[MAX_MODEL_NUM], hls::stream<PROB> P[MAX_MODEL_NUM], ap_uint<1> func) {
+void ProcessProb(hls::stream<Vector<MAX_MODEL_NUM, PROB> > &probs, hls::stream<Vector<MAX_MODEL_NUM, PROB> > &P, ap_uint<1> func) {
     if(func){
         AccumProb(probs, P);
     } else {
-        hls::stream<PROB> local_dis_128[128];
-        #pragma HLS STREAM variable=local_dis_128 depth=32
-        hls::stream<PROB> local_dis_64[64];
-        #pragma HLS STREAM variable=local_dis_64 depth=32
-        hls::stream<PROB> local_dis_32[32];
-        #pragma HLS STREAM variable=local_dis_32 depth=32
-        hls::stream<PROB> local_dis_16[16];
-        #pragma HLS STREAM variable=local_dis_16 depth=32
-        hls::stream<PROB> local_dis_8[8];
-        #pragma HLS STREAM variable=local_dis_8 depth=32
-        hls::stream<PROB> local_dis_4[4];
-        #pragma HLS STREAM variable=local_dis_4 depth=32
-        hls::stream<PROB> local_dis_2[2];
-        #pragma HLS STREAM variable=local_dis_2 depth=32
-        hls::stream<uint32_t> label_128[128];
-        #pragma HLS STREAM variable=label_128 depth=32
-        hls::stream<uint32_t> label_64[64];
-        #pragma HLS STREAM variable=label_64 depth=32
-        hls::stream<uint32_t> label_32[32];
-        #pragma HLS STREAM variable=label_32 depth=32
-        hls::stream<uint32_t> label_16[16];
-        #pragma HLS STREAM variable=label_16 depth=32
-        hls::stream<uint32_t> label_8[8];
-        #pragma HLS STREAM variable=label_8 depth=32
-        hls::stream<uint32_t> label_4[4];
-        #pragma HLS STREAM variable=label_4 depth=32
-        hls::stream<uint32_t> label_2[2];
-        #pragma HLS STREAM variable=label_2 depth=32
+        hls::stream<Vector<128, PROB> > dis_128;
+        #pragma HLS STREAM variable=dis_128 depth=16
+        hls::stream<Vector<64, PROB> > dis_64;
+        #pragma HLS STREAM variable=dis_64 depth=16
+        hls::stream<Vector<32, PROB> > dis_32;
+        #pragma HLS STREAM variable=dis_32 depth=16
+        hls::stream<Vector<16, PROB> > dis_16;
+        #pragma HLS STREAM variable=dis_16 depth=16
+        hls::stream<Vector<8, PROB> > dis_8;
+        #pragma HLS STREAM variable=dis_8 depth=16
+        hls::stream<Vector<4, PROB> > dis_4;
+        #pragma HLS STREAM variable=dis_4 depth=16
+        hls::stream<Vector<2, PROB> > dis_2;
+        #pragma HLS STREAM variable=dis_2 depth=16
+        hls::stream<Vector<128, uint32_t> > label_128;
+        #pragma HLS STREAM variable=label_128 depth=16
+        hls::stream<Vector<64, uint32_t> > label_64;
+        #pragma HLS STREAM variable=label_64 depth=16
+        hls::stream<Vector<32, uint32_t> > label_32;
+        #pragma HLS STREAM variable=label_32 depth=16
+        hls::stream<Vector<16, uint32_t> > label_16;
+        #pragma HLS STREAM variable=label_16 depth=16
+        hls::stream<Vector<8, uint32_t> > label_8;
+        #pragma HLS STREAM variable=label_8 depth=16
+        hls::stream<Vector<4, uint32_t> > label_4;
+        #pragma HLS STREAM variable=label_4 depth=16
+        hls::stream<Vector<2, uint32_t> > label_2;
+        #pragma HLS STREAM variable=label_2 depth=16
         
-        GetLabel_128(probs, local_dis_128, label_128);
-        GetLabel_64(local_dis_128, label_128, local_dis_64, label_64);
-        GetLabel_32(local_dis_64, label_64, local_dis_32, label_32);
-        GetLabel_16(local_dis_32, label_32, local_dis_16, label_16);
-        GetLabel_8(local_dis_16, label_16, local_dis_8, label_8);
-        GetLabel_4(local_dis_8, label_8, local_dis_4, label_4);
-        GetLabel_2(local_dis_4, label_4, local_dis_2, label_2);
-        GetLabel_1(local_dis_2, label_2, P);
+        GetLabel_128(probs, dis_128, label_128);
+        GetLabel<128, 64>(dis_128, label_128, dis_64, label_64);
+        GetLabel<64, 32>(dis_64, label_64, dis_32, label_32);
+        GetLabel<32, 16>(dis_32, label_32, dis_16, label_16);
+        GetLabel<16, 8>(dis_16, label_16, dis_8, label_8);
+        GetLabel<8, 4>(dis_8, label_8, dis_4, label_4);
+        GetLabel<4, 2>(dis_4, label_4, dis_2, label_2);
+        GetLabel_1(dis_2, label_2, P);
     }
 }
 
-void Update(hls::stream<PROB> P[MAX_MODEL_NUM], PRIOR next_priors[MAX_MODEL_NUM], MEANS next_means[MAX_MODEL_NUM][DIM], VARS next_vars[MAX_MODEL_NUM][DIM], ap_uint<9> count[MAX_MODEL_NUM], hls::stream<DATA> &data2, ap_uint<1> func){
+void Update(hls::stream<Vector<MAX_MODEL_NUM, PROB> > &P, PRIOR next_priors[MAX_MODEL_NUM], MEANS next_means[MAX_MODEL_NUM][DIM], VARS next_vars[MAX_MODEL_NUM][DIM], ap_uint<9> count[MAX_MODEL_NUM], hls::stream<DATA> &data2, ap_uint<1> func){
     UpdateF:for(uint32_t i=0; i<DATA_NUM; i++) {
         DATA sample[3];
         Update1:for(uint32_t i=0; i<3; i++){
             data2.read(sample[i]);
         }
 
+        Vector<MAX_MODEL_NUM, PROB> local_P;
+        P.read(local_P);
         Update2:for(uint32_t i=0; i<MAX_MODEL_NUM; i++){
-        //#pragma HLS UNROLL factor=MAX_MODEL_NUM
-            PROB tmp;
-            P[i].read(tmp);
-            if((func==0)&&(tmp==1)){
+            if((func==0)&&(local_P.vec[i]==1)){
                 count[i]++;
             }
-            next_priors[i] += tmp;
+            local_P.vec[i] += next_priors[i];
             Update3:for(uint32_t d=0; d<DIM; d++) {
                 #pragma HLS UNROLL factor=3
-                next_means[i][d] += tmp*sample[d];
-                next_vars[i][d] += tmp*sample[d]*sample[d];
+                next_means[i][d] += local_P.vec[i]*sample[d];
+                next_vars[i][d] += local_P.vec[i]*sample[d]*sample[d];
             }
         }
 
@@ -296,9 +206,9 @@ void EMCore(DATA _data[MAX_MODEL_NUM*3],
     hls::stream<DATA> data2;
     #pragma HLS STREAM variable=data2 depth=64
 
-    hls::stream<PROB> probs[MAX_MODEL_NUM];
+    hls::stream<Vector<MAX_MODEL_NUM, PROB> > probs;
     #pragma HLS STREAM variable=probs depth=16
-    hls::stream<PROB> P[MAX_MODEL_NUM];
+    hls::stream<Vector<MAX_MODEL_NUM, PROB> > P;
     #pragma HLS STREAM variable=P depth=16
 
     GetData(_data, data, data2);
